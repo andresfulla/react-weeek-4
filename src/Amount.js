eaef8dd6dec0ce2df4a350eeed45f40a141c48f1
Greg Hurrell <greg.hurrell@liferay.com>
2019-11-27 17:37:55 +0100

    feat(step 4): show red outline if negative

--- src/Amount.js
+++ src/Amount.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import './Amount.css';
 
 export default class Amount extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {isNegative: false};
+  }
+
+  _onChange = event => {
+    const isNegative = parseFloat(event.currentTarget.value) < 0;
+
+    this.setState({isNegative});
+  }
+
   render() {
     return (
-        <label>
+        <label className="amount">
           <span>{this.props.name}</span>
-          <input placeholder="0" step="0.1" type="number" />
+          <input
+            className={this.state.isNegative ? 'negative' : ''}
+            onChange={this._onChange}
+            placeholder="0"
+            step="0.1"
+            type="number"
+          />
         </label>
     );
   }
