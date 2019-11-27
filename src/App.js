db3245902fbad257ea0f0150082cccbe08a5af13
Greg Hurrell <greg.hurrell@liferay.com>
2019-11-27 17:18:29 +0100

    feat(step 0): add Euros input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <label>
+        <span>Euros</span>
+        <input placeholder="0" step="0.1" type="number" />
+      </label>
     </div>
   );
 }
