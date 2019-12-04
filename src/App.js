1d9d12851e716b3f1dfb69d8ef483c1eb7054d46
Andrés Fulla <andres.fulla@liferay.com>
2019-12-04 19:33:25 +0100

    feat(step 9): extract logic to <Converter />

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Amount from './Amount';
+import Converter from './Converter';
 import './App.css';
 import ConverterContext from './ConverterContext';
-
-const SECOND = 1000; // milliseconds
-const CRASH_DELAY = 5 * SECOND;
 
 function exchangeRate() {
   return Math.random() * 10000;
@@ -17,27 +14,8 @@ export default class App extends React.Component {
     this.state = {
       exchangeRate: exchangeRate(),
       theme: 'light',
-      value: 0
+      value: 0,
     };
-  }
-
-  _onChange = value => {
-    this.setState({
-      exchangeRate: exchangeRate(),
-      value,
-    });
-
-    clearTimeout(this._timer);
-
-    this._timer = setTimeout(() => {
-      this.setState({
-        exchangeRate: 0,
-      });
-    }, CRASH_DELAY)
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this._timer);
   }
 
   render() {
@@ -48,23 +26,14 @@ export default class App extends React.Component {
             <span>Theme</span>
             <select
               onChange={event => this.setState({theme: event.target.value})}
-              value={this.state.theme}
-            >
+              value={this.state.theme}>
               <option value="dark">Dark</option>
               <option value="light">Light</option>
             </select>
           </label>
-          <Amount
-            name="Euros"
-            onChange={this._onChange}
-            value={this.state.value}
-          />
-          <Amount
-            disabled
-            name="$BTC"
-            onChange={this._onChange}
-            value={(this.state.value * this.state.exchangeRate).toFixed(4)}
-          />
+          <div>
+            <Converter cryptoName="$BTC" exchangeRate={0.5} />
+          </div>
         </div>
       </ConverterContext.Provider>
     );
---
+++ src/Converter.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Amount from './Amount';
+import './Converter.css';
+
+export default class Converter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0,
+    };
+  }
+
+  _onChange = value => {
+    this.setState({
+      value,
+    });
+  };
+
+  render() {
+    const {cryptoName, exchangeRate} = this.props;
+
+    return (
+      <div className="converter">
+        <Amount
+          name="Euros"
+          onChange={this._onChange}
+          value={this.state.value}
+        />
+        <Amount
+          disabled
+          name={cryptoName}
+          onChange={this._onChange}
+          value={(this.state.value * exchangeRate).toFixed(4)}
+        />
+      </div>
+    );
+  }
+}
